a59b6014ec94a1f942eeb5cb58aca80cd7251672
Courtney Greer <courtneygreer@Courtneys-MacBook-Pro.local>
2025-01-15 23:49:31 -0500

    remove test page

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { HashRouter, Routes, Route, Link } from "react-router-dom";
 import Index from "./pages/Index";
 import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
-import TestData from "./pages/TestData";
 import About from "./pages/About";
 
 
@@ -33,9 +32,6 @@ const Navigation = () => (
           <Link to="/contact" className="text-matrix-green hover:brightness-125 transition-all">
             Contact
           </Link>
-          <Link to="/testdata" className="text-matrix-green hover:brightness-125 transition-all">
-            Test
-          </Link>
         </div>
       </div>
     </div>
@@ -53,7 +49,6 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/testdata" element={<TestData />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </HashRouter>
